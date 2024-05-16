5d242e578de80b002489898af399d067baad8696
Musab <67883529+Musab45@users.noreply.github.com>
2024-05-16 19:32:59 +0500

    Create

---
+++ src/main/java/com/example/lab13practice/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.lab13practice;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
